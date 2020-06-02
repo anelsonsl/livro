aa6137de40f0d76b0f628361c22a7479881f6e8e
A. Nelson <65572920+anelsonsl@users.noreply.github.com>
2020-06-02 10:53:57 +0200

    010620

--- iniciarjogo.py
+++ iniciarjogo.py
@@ -7,7 +7,7 @@ def lancardado(d):
     dado=0
     for i in range(d):
         dado += randint(1,6)
-    return dado;
+    return dado
 
 valor={
     'hm':[0,0], #Minha Habilidae
@@ -15,8 +15,20 @@ valor={
     'sm':[0,0], #Minha Sorte
     'mm':[0,0], #Minha Magia
     'ouro':0,   #Meu Ouro
-    'magia':[[]],#Meus feiticos existe 12  e depois quantos tenho de cada um
-    'equipamneto':[]}#Meus material tipo texto
+    'magia':{'Copia de criatura':0,
+            'Percepcao extra sensorial':0,
+            'Fogo':0,
+            'Ouro dos Tolos':0,
+            'Ilusao':0,
+            'Levitacao':0,
+            'Sorte':0,
+            'Escudo':0,
+            'Habilidade':0,
+            'Energia':0,
+            'Forca':0,
+            'Fraquesa':0
+            }, #Meus feiticos existe 12  e depois quantos tenho de cada um
+    'equipamneto':['espada','armadura de couro','lanterna','Mochila']}#Meus material tipo texto
 
 linha()
 
@@ -42,3 +54,7 @@ valor['sm'][1]=valor['sm'][0]
 valor['mm'][1]=valor['mm'][0]
 
 print(valor)
+linha()
+#Escolher os feiticos
+
+print(valor['magia'])
